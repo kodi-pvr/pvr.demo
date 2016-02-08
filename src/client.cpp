d99a7c055bccb813644335fbcc8d0f8378c81467
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2016-02-08 16:26:51 +0100

    [API 5.0.0] Remove GetCurrentClientChannel

--- src/client.cpp
+++ src/client.cpp
@@ -243,11 +243,6 @@ void CloseLiveStream(void)
   m_bIsPlaying = false;
 }
 
-int GetCurrentClientChannel(void)
-{
-  return m_currentChannel.iUniqueId;
-}
-
 bool SwitchChannel(const PVR_CHANNEL &channel)
 {
   CloseLiveStream();
