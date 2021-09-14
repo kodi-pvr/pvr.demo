c1a0b5976067eb7b7c8fd7351a6c488616ae2f80
Alwin Esch <alwin.esch@web.de>
2021-09-15 00:24:54 +0200

    update copyright year to 2021

--- src/PVRDemo.cpp
+++ src/PVRDemo.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2011-2020 Team Kodi (https://kodi.tv)
+ *  Copyright (C) 2011-2021 Team Kodi (https://kodi.tv)
  *  Copyright (C) 2011 Pulse-Eight (http://www.pulse-eight.com/)
  *
  *  SPDX-License-Identifier: GPL-2.0-or-later
